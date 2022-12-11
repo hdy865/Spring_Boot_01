17b21f4c4c66fcc9c036f07285d6ba524d94c4da
hdy865 <yangsihyun2865@gmail.com>
2022-12-11 20:23:42 +0900

    Part03

---
+++ board/src/main/java/org/zerock/board/entity/Board.java
@@ -0,0 +1,26 @@
+package org.zerock.board.entity;
+
+import javax.persistence.*;
+
+import lombok.*;
+
+@Entity
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@ToString(exclude = "writer") //@ToString은 항상 exclude
+public class Board extends BaseEntity{
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long bno;
+	
+	private String title;
+	
+	private String content;
+	
+	@ManyToOne
+	private Member writer; //연관관계 지정
+	
+}
---
+++ board/src/main/java/org/zerock/board/entity/Member.java
@@ -0,0 +1,23 @@
+package org.zerock.board.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+import lombok.*;
+
+@Entity
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@ToString
+public class Member extends BaseEntity{
+	
+	@Id
+	private String email;
+	
+	private String passwrod;
+	
+	private String name;
+
+}
